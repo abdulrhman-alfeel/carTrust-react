ee659a9cf129707025dc8c568e3c31ffa1eaceca
حمزه الفيل <abrhman2010@gmail.com>
2024-01-20 05:11:04 +0300

    added channel name

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,17 +7,18 @@ import Header from "../components/Sections/Header";
 import Projects from "../components/Sections/Projects";
 import Services from "../components/Sections/Services";
 import Pricing from "../components/Sections/Pricing";
+import Dashboard from "./Dashboard";
 
 export default function Landing() {
   return (
     <div style={{overflow:"hidden"}}>
-      <TopNavbar />
-      <Header />
-      <Services />
-      <Projects />
-      <Blog />
-    
-      <Footer />
+      {/* <TopNavbar /> */}
+      {/* <Header /> */}
+      {/* <Services /> */}
+      {/* <Projects /> */}
+      {/* <Blog /> */}
+      <Dashboard />
+      {/* <Footer /> */}
     </div>
   );
 }
